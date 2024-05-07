9303dd88cac398cf9965685eba11ec3824204a0d
ahshanz <95.ahshan@gmail.com>
2024-05-07 18:01:25 +0530

    properties added, api routes created, db connected, users and properties model created

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,18 +1,22 @@
+import Navbar from '@/components/Navbar'
 import '@/assets/styles/global.css';
 
-export const metadata ={
-    title: 'PropetyPulse | Find the perfect Rental',
-    description: 'Find your dream rental property',
-    keywords: 'rental, find rentals, find properties'
-}
+export const metadata = {
+  title: 'PropetyPulse | Find the perfect Rental',
+  description: 'Find your dream rental property',
+  keywords: 'rental, find rentals, find properties',
+};
 
-import React from "react";
+import React from 'react';
+import Footer from '@/components/Footer';
 
 const MainLayout = ({ children }) => {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body>
-        <div>{children}</div>
+        <Navbar />
+        <main>{children}</main>
+        <Footer/>
       </body>
     </html>
   );
