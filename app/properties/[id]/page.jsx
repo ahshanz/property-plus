8b382bb462d0ab86bb035f9eba6925d3c17f0629
ahshanz <95.ahshan@gmail.com>
2024-05-08 15:15:34 +0530

    Header image component added to propery page

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -2,6 +2,7 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'next/navigation';
 import { fetchProperty } from '@/utils/requests';
+import PropertyHeaderImage from '@/components/PropertyHeaderImage';
 
 const PropertyPage = () => {
   const { id } = useParams();
@@ -17,16 +18,42 @@ const PropertyPage = () => {
         setProperty(property);
       } catch (error) {
         console.log('Error fetching property', error);
-      }finally{
+      } finally {
         setLoading(false);
       }
     };
-    if (property === null){
-      fecthPropertyData()
+    if (property === null) {
+      fecthPropertyData();
     }
   }, [id, property]);
+  if (!property && !loading) {
+    return (
+      <h1 className='text-center text-2xl font-bold mt-10'>
+        Property Not Found
+      </h1>
+    );
+  }
 
-  return <div>PropertyPage</div>;
+  return (
+    <>
+      {!loading && property && (
+        <>
+          <PropertyHeaderImage image={property.images[0]} />
+
+          <section>
+            <div class='container m-auto py-6 px-6'>
+              <a
+                href='/properties.html'
+                class='text-blue-500 hover:text-blue-600 flex items-center'
+              >
+                <i class='fas fa-arrow-left mr-2'></i> Back to Properties
+              </a>
+            </div>
+          </section>
+        </>
+      )}
+    </>
+  );
 };
 
 export default PropertyPage;
