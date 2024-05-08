6bf67de8a320629d3021d92624348017011e90c6
ahshanz <95.ahshan@gmail.com>
2024-05-08 14:10:10 +0530

    page for single properties added with fecth

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+'use client';
+import { useEffect, useState } from 'react';
+import { useParams } from 'next/navigation';
+import { fetchProperty } from '@/utils/requests';
 
 const PropertyPage = () => {
-  return (
-    <div>PropertyPage</div>
-  )
-}
+  const { id } = useParams();
 
-export default PropertyPage
+  const [property, setProperty] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fecthPropertyData = async () => {
+      if (!id) return;
+      try {
+        const property = await fetchProperty(id);
+        setProperty(property);
+      } catch (error) {
+        console.log('Error fetching property', error);
+      }finally{
+        setLoading(false);
+      }
+    };
+    if (property === null){
+      fecthPropertyData()
+    }
+  }, [id, property]);
+
+  return <div>PropertyPage</div>;
+};
+
+export default PropertyPage;
