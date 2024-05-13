5018bb0e6723559e2662f6d972743a4f8f26a0e9
ahshanz <95.ahshan@gmail.com>
2024-05-13 17:23:42 +0530

    auth provider

---
+++ components/AuthProvider.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {SessionProvider} from 'next-auth/react'
+
+const AuthProvider = ({children}) => {
+  return (
+    <SessionProvider>{children}</SessionProvider>
+  )
+}
+
+export default AuthProvider
