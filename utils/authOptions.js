6bed076b080bc47b1c3ca110fbbed225d8398c8d
ahshanz <95.ahshan@gmail.com>
2024-05-15 12:08:01 +0530

    auth options set for siginIn and session

--- utils/authOptions.js
+++ utils/authOptions.js
@@ -1,7 +1,9 @@
 import GoogleProvider from 'next-auth/providers/google';
+import connectDB from '@/config/database';
+import User from '@/models/User';
 
 export const authOptions = {
-  provider: [
+  providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
@@ -17,17 +19,34 @@ export const authOptions = {
   callbacks:{
     //Inovoked on successful signin
     async signIn({profile}){
+      console.log(profile);
         // 1. Connect to database
+        await connectDB();
         // 2. Check if user exists
+        const  userExists = await User.findOne({ email:profile.email})
         // 3. If not, then add user to database
-        // 4. Return to allow sign in
+        if(!userExists){
+          // Truncate user name if too long
+          const username = profile.name.slice(0,20);
 
+          await User.create({
+            email:profile.email,
+            username,
+            image:profile.picture
+          })
+        }
+        // 4. Return to allow sign in
+        console.log(profile.email);
+      return true;
     },
     // Modify the session object
     async session({session}){
         // 1. Get user from database
+        const user =await User.findOne({email:session.user.email})
         // 2. Assing the user id to the session
+        session.user.id = user._id.toString();
         // 3. Return session
+        return session;
     }
   }
 };
